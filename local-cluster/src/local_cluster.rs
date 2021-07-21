bd41ea7bc0cd1f11c3609ac2739edc2a584660f0
Michael Vines <mvines@gmail.com>
2021-07-20 22:25:13 -0700

    Extract tower storage details from Tower struct

--- local-cluster/src/local_cluster.rs
+++ local-cluster/src/local_cluster.rs
@@ -49,7 +49,6 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-#[derive(Debug)]
 pub struct ClusterConfig {
     /// The validator config that should be applied to every node in the cluster
     pub validator_configs: Vec<ValidatorConfig>,
